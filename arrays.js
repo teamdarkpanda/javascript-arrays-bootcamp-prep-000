8b68851078ee51d3b066219bac1a17f2b2540047
Ankur Puri <apuri@iit.edu>
2018-05-14 14:29:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,5 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  
+  var newArray = array.slice(1)
+  return newArray
 }
